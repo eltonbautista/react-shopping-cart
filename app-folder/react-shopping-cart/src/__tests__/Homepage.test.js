1a91ec414593d496c2cf53ae21fcfc5edbb28e4f
Elton Bautista <eltonbestaha@gmail.com>
2022-05-17 08:37:51 -0600

    Created Homepage component

---
+++ app-folder/react-shopping-cart/src/__tests__/Homepage.test.js
@@ -0,0 +1,10 @@
+import React, { ReactDOM } from "react";
+import { getByTestId, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";  // optional
+import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
+import Homepage from "../components/homepage/Homepage";
+
+describe('Tests for Homepage', () => {
+  it();
+});
