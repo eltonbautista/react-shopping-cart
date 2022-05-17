6f662718af9a777d712feeab99d0b066a3973c2d
Elton Bautista <eltonbestaha@gmail.com>
2022-05-17 13:59:54 -0600

    Created a factory function module for creating my product objects

---
+++ app-folder/react-shopping-cart/src/modules/factory-function-products.js
@@ -0,0 +1,31 @@
+const factoryFunctionProducts = (name, description, price, imgSrc, imgAlt, isInCart) => {
+
+  const isInCartFunction = function(bool) {
+    return this.isInCart = bool;
+  }
+
+  return {
+    name,
+    description,
+    price: `$${price}`,
+    imgSrc,
+    imgAlt,
+    isInCart,
+    isInCartFunction,
+  }
+}
+
+const firstProduct = factoryFunctionProducts('death bolt', 'this is deadly', 329.99, 'url', 'black lightning', false);
+console.log(firstProduct);
+
+const arrayOfProducts =
+ [
+   [],
+   [],
+   [],
+ ]
+
+//  const [damage, cultivation, movement] = arrayOfProducts;
+
+export default arrayOfProducts;
+export {firstProduct};
