82f44ea618e1199085b0f3a4b0e8087553f6c95e
Elton Bautista <eltonbestaha@gmail.com>
2022-05-17 12:02:00 -0600

    Created new Product component file and corresponding test file

---
+++ app-folder/react-shopping-cart/src/__tests__/__componentTests__/Product.test.js
@@ -0,0 +1,17 @@
+import React, { ReactDOM } from "react";
+import { getByTestId, render, screen, } from "@testing-library/react";
+import "@testing-library/jest-dom";  // optional
+import userEvent from "@testing-library/user-event";
+import { act, createRenderer } from "react-dom/test-utils";
+import renderer from 'react-test-renderer';
+
+import Product from "../../components/products/Product";
+
+describe('Tests for Product', () => {
+
+  it('Making sure product renders properly', () => {
+
+  })
+
+  
+});
