845b10aa0b57eb4add66be42961d611969e70bdc
Elton Bautista <eltonbestaha@gmail.com>
2022-05-17 08:02:36 -0600

    Added my first test file and first test

--- app-folder/react-shopping-cart/src/App.js
+++ app-folder/react-shopping-cart/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" data-testid='App'>
+      Shopping Cart
     </div>
   );
 }
---
+++ app-folder/react-shopping-cart/src/__tests__/App.test.js
@@ -0,0 +1,14 @@
+import React, { ReactDOM } from "react";
+import { getByTestId, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";  // optional
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+import { act } from "react-dom/test-utils";
+
+describe('Testing App component', () => {
+  it('Initial test to make sure App is rendering correctly', () => {
+    const { getByTestId } = render(<App />);
+
+    expect(screen.getByTestId('App').textContent).toMatch(/shopping cart/i);
+  })
+})
