23fd84a6941f4576030aaf56bd83201fe7205ad6
Elton Bautista <eltonbestaha@gmail.com>
2022-05-18 08:59:55 -0600

    Moved my Routing code into a separate component This allows me to shorten my test, as well as the code in my index.js

---
+++ app-folder/react-shopping-cart/src/RouteSwitch.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Homepage from './components/homepage/Homepage';
+import ProductPage from './components/product-components/ProductPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProductContainer from './components/product-components/ProductContainer';
+
+const productChildren = [<ProductContainer products={['Category 1']}  />,
+<ProductContainer products={['Category 2']}  />,
+<ProductContainer products={['Category 3']} />]
+
+  const RouteSwitch = () => {
+
+    return (    
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App title='Murim Shopping Cart' />}>
+            <Route path='homepage' element={<Homepage />} />
+            <Route path='product-category-zero' element={<ProductPage productHeading='Damage' children={productChildren[0]} />} />
+            <Route path='product-category-one' element={<ProductPage productHeading='Cultivation' children={productChildren[1]} />} />
+            <Route path='product-category-two' element={<ProductPage productHeading='Movement' children={productChildren[2]} />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>);
+  }
+
+  export default RouteSwitch;
+
--- app-folder/react-shopping-cart/src/index.js
+++ app-folder/react-shopping-cart/src/index.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Homepage from './components/homepage/Homepage';
-import ProductPage from './components/product-components/ProductPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ProductContainer from './components/product-components/ProductContainer';
-
-
-const productChildren = [<ProductContainer products={['Category 1']}  />,
-<ProductContainer products={['Category 2']}  />,
-<ProductContainer products={['Category 3']} />]
+import RouteSwitch from './RouteSwitch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App title='Murim Shopping Cart' />}>
-          <Route path='homepage' element={<Homepage />} />
-          <Route path='product-category-zero' element={<ProductPage productHeading='Damage' children={productChildren[0]} />} />
-          <Route path='product-category-one' element={<ProductPage productHeading='Cultivation' children={productChildren[1]} />} />
-          <Route path='product-category-two' element={<ProductPage productHeading='Movement' children={productChildren[2]} />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouteSwitch />
   </React.StrictMode>
 );
