0d81c629e8cfd20d92f0825d9a3d8679da25d477
Elton Bautista <eltonbestaha@gmail.com>
2022-07-20 07:21:42 -0600

    Restyling

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -9,8 +9,8 @@ const Homepage = function Homepage(props) {
   // An object to store information that's used in the JSX
   const fillInformation = {
     descriptionHeader: 'Our Vision...',
-    description: 
-    'We are a company dedicated to delivering modernity in essence... Whilst maintaining the value of the past. We understand that fashion is a statement and that everybody has their own unique sense of individuality. We embrace those who embrace uniqueness and prioritize revolutionizing the fashion fashion fashion world..',
+    description:
+      'We are a company dedicated to delivering modernity in essence... Whilst maintaining the value of the past. We understand that fashion is a statement and that everybody has their own unique sense of individuality. We embrace those who embrace uniqueness and prioritize revolutionizing the fashion fashion fashion world..',
     imgOneSrc: '',
     imgOneAlt: 'store',
     imgTwoSrc: '',
@@ -18,17 +18,17 @@ const Homepage = function Homepage(props) {
     extraInfoHeading: 'Come As You Are',
     extraInfo: 'One of the core foundations of our company is our motto: COME AS YOU ARE. We are a company with various differing backgrounds, this is why we emphasize the value of embracing those who may be different from us. As others have given us a chance, we have focused creating this company to give others that opportunity and we are doing this through our award-winning designers who have come from all depths of the world to create the ultimate clothing - no.. Fashion Coporation!',
     moreInfo: '"COME AS YOU ARE"'
-  }
+  };
   const [vintage, blueLights, caya] = [
-    {backgroundImage: `url(${homepageImages[4].src})`},
-    {backgroundImage: `url(${homepageImages[5].src})`},
-    {backgroundImage: `url(${homepageImages[6].src})`}
-  ]
+    { backgroundImage: `url(${homepageImages[4].src})` },
+    { backgroundImage: `url(${homepageImages[5].src})` },
+    { backgroundImage: `url(${homepageImages[6].src})` }
+  ];
 
 
 
   return (
-    <div style={blueLights} id="homepage" data-testid='homepage' className={styles.homepageDiv}>
+    <div id="homepage" data-testid='homepage' className={styles.homepageDiv}>
       <div className={`homepage ${styles['about-info']}`}>
         <h1 className={styles['description-one-header']}>{fillInformation.descriptionHeader}</h1>
         <p className={styles['description-one']}>{fillInformation.description}</p>
