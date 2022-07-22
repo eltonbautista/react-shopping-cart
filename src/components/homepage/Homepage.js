510202d6fff0c64cbe975e8a3d3b0300ff4ad9f7
Elton Bautista <eltonbestaha@gmail.com>
2022-07-22 07:31:24 -0600

    Refactored and cleaned up, also styled a bit

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,43 +1,28 @@
 import React from "react";
 import ImgInDiv from "./ImgInDiv";
 import styles from '../../modules/styling-modules/Homepage.module.css';
-import Carousel from "./ProductCarousel";
 import HomepageFooter from "./HomepageFooter";
 import { homepageImages } from "../../modules/preload";
 
 const Homepage = function Homepage(props) {
   // An object to store information that's used in the JSX
-  const fillInformation = {
-    descriptionHeader: 'Our Vision...',
-    description:
-      'We are a company dedicated to delivering modernity in essence... Whilst maintaining the value of the past. We understand that fashion is a statement and that everybody has their own unique sense of individuality. We embrace those who embrace uniqueness and prioritize revolutionizing the fashion fashion fashion world..',
-    imgOneSrc: '',
-    imgOneAlt: 'store',
-    imgTwoSrc: '',
-    imgTwoAlt: '',
-    extraInfoHeading: 'Come As You Are',
-    extraInfo: 'One of the core foundations of our company is our motto: COME AS YOU ARE. We are a company with various differing backgrounds, this is why we emphasize the value of embracing those who may be different from us. As others have given us a chance, we have focused creating this company to give others that opportunity and we are doing this through our award-winning designers who have come from all depths of the world to create the ultimate clothing - no.. Fashion Coporation!',
-    moreInfo: '"COME AS YOU ARE"'
-  };
-  const [vintage, blueLights, caya] = [
+  const [vintage] = [
     { backgroundImage: `url(${homepageImages[4].src})` },
-    { backgroundImage: `url(${homepageImages[5].src})` },
-    { backgroundImage: `url(${homepageImages[6].src})` }
   ];
-
-
 
   return (
     <div id="homepage" data-testid='homepage' className={styles.homepageDiv}>
       <div className={`homepage ${styles['about-info']}`}>
-        <h1 className={styles['description-one-header']}>{fillInformation.descriptionHeader}</h1>
-        <p className={styles['description-one']}>{fillInformation.description}</p>
+        <h1 className={styles['description-one-header']}>Our Vision...</h1>
+        <p className={styles['description-one']}>
+          We are a company dedicated to delivering <strong>modernity</strong> in essence, while maintaining the value of the past. We understand that fashion is a <strong>statement</strong>, and that everybody has their own <em>unique</em> sense of <strong>individuality</strong>. We embrace those who embrace <strong>uniqueness</strong> and prioritize revolutionizing the fashion world..
+        </p>
       </div>
       <ImgInDiv backgroundImage={vintage} imgStyling={styles.homepageImage} stylingClass={styles['store-image-div']} src={''} alt={''} />
       <div className={`homepage ${styles['extra-info']}`}>
-        <h3 className={styles['description-two-header']}>{fillInformation.extraInfoHeading}</h3>
+        <h3 className={styles['description-two-header']}>Come As You Are</h3>
         <p className={styles['description-two']}>
-          {fillInformation.extraInfo}
+          One of the core foundations of our company is our motto:<strong>COME AS YOU ARE</strong>. We are a company with various differing backgrounds. This is why we emphasize the value of embracing those who may be <em>different</em> from us. As many have given us a chance, our focus is to give others that same opportunity. We aim to deliver our goals through the <em>brilliance</em> of our <strong>award-winning</strong> designers, individuals who have come from all parts of the world.
         </p>
       </div>
       <HomepageFooter styles={styles} />
